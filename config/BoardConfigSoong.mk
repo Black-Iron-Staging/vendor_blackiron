a7dd156f89603e096317481fa9539d629aad308f
inferno0230 <adityachoudhury0230@gmail.com>
2023-04-12 01:00:31 +0530

    soong: Add TARGET_NEEDS_OPLUS_VENDOR_TAG
    Change-Id: Ica0675e3b812e32459d82ea3d648829a73f26be5
    (cherry picked from commit 11f751a76380eca5e5bd1b5db329ff9fc4c732df)

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -48,7 +48,8 @@ SOONG_CONFIG_blackironGlobalVars += \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
     camera_needs_client_info_lib \
-    uses_egl_display_array
+    uses_egl_display_array \
+    needs_oplus_tag
 
 SOONG_CONFIG_NAMESPACES += blackironNvidiaVars
 SOONG_CONFIG_blackironNvidiaVars += \
@@ -68,7 +69,7 @@ endif
 # Soong bool variables
 SOONG_CONFIG_blackironGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
 SOONG_CONFIG_blackironGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
-SOONG_CONFIG_evolutionGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
+SOONG_CONFIG_blackironGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_blackironGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_blackironGlobalVars_gralloc_handle_has_custom_content_md_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE)
 SOONG_CONFIG_blackironGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
@@ -76,6 +77,7 @@ SOONG_CONFIG_blackironGlobalVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRA
 SOONG_CONFIG_blackironNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_blackironQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_blackironQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_blackironGlobalVars_needs_oplus_tag := $(TARGET_NEEDS_OPLUS_VENDOR_TAG)
 
 # Set default values
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
