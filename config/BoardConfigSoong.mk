2a172a0e37ee14c582ba8031ca99100058f728ed
ShevT <evgeny.shishkov@gmail.com>
2023-05-23 19:20:31 +0300

    vendor: soong: Add camera_needs_client_info_lib_oplus
    Change-Id: I075cdcae3b74104ba6923c151b1e92e4513e8996

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -87,6 +87,7 @@ SOONG_CONFIG_blackironGlobalVars_include_miui_camera := $(TARGET_INCLUDES_MIUI_C
 SOONG_CONFIG_blackironGlobalVars_camera_uses_newer_hidl_override_format := $(TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT)
 SOONG_CONFIG_blackironGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_blackironGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
+SOONG_CONFIG_blackironGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
 
 # Set default values
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
