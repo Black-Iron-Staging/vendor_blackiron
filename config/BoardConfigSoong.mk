b5c52e4e6dab137df803799b15f0bc918967064c
johnmart19 <johnivan19999@gmail.com>
2023-05-31 00:01:09 +0300

    soong: Add TARGET_INCLUDES_MIUI_CAMERA
    - Inspired from 11f751a76380eca5e5bd1b5db329ff9fc4c732df

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -49,7 +49,8 @@ SOONG_CONFIG_blackironGlobalVars += \
     target_trust_usb_control_disable \
     camera_needs_client_info_lib \
     uses_egl_display_array \
-    needs_oplus_tag
+    needs_oplus_tag \
+    include_miui_camera
 
 SOONG_CONFIG_NAMESPACES += blackironNvidiaVars
 SOONG_CONFIG_blackironNvidiaVars += \
@@ -78,6 +79,7 @@ SOONG_CONFIG_blackironNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEW
 SOONG_CONFIG_blackironQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_blackironQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_blackironGlobalVars_needs_oplus_tag := $(TARGET_NEEDS_OPLUS_VENDOR_TAG)
+SOONG_CONFIG_blackironGlobalVars_include_miui_camera := $(TARGET_INCLUDES_MIUI_CAMERA)
 
 # Set default values
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
