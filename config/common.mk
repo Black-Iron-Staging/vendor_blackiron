Project-Awaken/android_vendor_awaken

overlay: Move out CertifiedPropsOverlay
* Also include our certification repo while we're at it.

Change-Id: I55c23ee67b11ff79e64a3407f2a28ebe795ee295

config/common.mk
@@ -251,16 +251,13 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/awaken/overlay/no-rro
 
 PRODUCT_PACKAGES += \
-    CertifiedPropsOverlay \
     CustomPixelLauncherOverlay \
     DocumentsUIOverlay \
     ImmersiveNavigationOverlay \
     NetworkStackOverlay \
     NexusLauncherResOverlay
 
-PRODUCT_COPY_FILES += \
-    vendor/awaken/config/config-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/overlay/config/config.xml
-
+include vendor/awaken/certification/config.mk
 include vendor/awaken/config/branding.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk

Black-Iron-Staging/vendor_blackiron

config/common.mk
@@ -196,11 +196,7 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/blackiron/overlay/common \
     vendor/blackiron/overlay/no-rro
 
-PRODUCT_PACKAGES += \
-    CertifiedPropsOverlay
-
-PRODUCT_COPY_FILES += \
-    vendor/blackiron/config/config-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/overlay/config/config.xml
+include vendor/pixelstar/certification/config.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 