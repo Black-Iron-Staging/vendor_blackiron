a560c56d1485819edd1a74670b2a08fe05e6789f
ralf979 <ralf979@gmail.com>
2024-02-22 16:41:50 +0100

    switch to Project Matrixx Gapps

--- config/common.mk
+++ config/common.mk
@@ -218,8 +218,8 @@ PRODUCT_PACKAGES += \
 # Versioning
 include vendor/blackiron/config/version.mk
 
-# GApps
-WITH_GMS := true
-$(call inherit-product-if-exists, vendor/google/gms/config.mk)
-$(call inherit-product-if-exists, vendor/google/pixel/config.mk)
+# GAPPS
+ifeq ($(WITH_GMS),true)
+$(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
+endif
 
