e5ba2298e45d6adb762cdac09fe98eceb039f6c8
ralf979 <ralf979@gmail.com>
2024-04-17 08:38:43 +0200

    blackiron: update  bootanimation path

--- config/common.mk
+++ config/common.mk
@@ -29,7 +29,7 @@ endif
 
 
 # Bootanimation
-$(call inherit-product, vendor/blackiron/config/bootanimation.mk)
+$(call inherit-product, vendor/lineage/config/bootanimation.mk)
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
@@ -126,11 +126,7 @@ PRODUCT_PACKAGES += \
 endif
 
 PRODUCT_PACKAGES += \
-    LineageSettingsProvider \
-    Updater
-
-PRODUCT_COPY_FILES += \
-    vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
+    LineageSettingsProvider
 
 # Config
 PRODUCT_PACKAGES += \
