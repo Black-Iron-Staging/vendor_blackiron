febec7f99003ac9cd87001f24df7a74a295ec81b
ralf979 <ralf979@gmail.com>
2024-04-18 10:57:44 +0200

    blackiron: update again zip package name

--- config/version.mk
+++ config/version.mk
@@ -7,7 +7,7 @@ BLACKIRON_BUILDTYPE ?= UNOFFICIAL
 BLKI_VERSION := 4.3
 
 # Internal version
-LINEAGE_VERSION := Blackiron-$(BLKI_VERSION)-$(LINEAGE_BUILD)-$(PRODUCT_VERSION_MAJOR)-$(shell date +%Y%m%d)-$(shell date -u +%H)$(shell date -u +%M)-$(BLACKIRON_BUILDTYPE)
+LINEAGE_VERSION := Blackiron-$(BLKI_VERSION)_$(LINEAGE_BUILD)-$(PRODUCT_VERSION_MAJOR)-$(shell date +%Y%m%d)-$(shell date -u +%H)$(shell date -u +%M)-$(BLACKIRON_BUILDTYPE)
 
 # Display version
 LINEAGE_DISPLAY_VERSION := Blackiron-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_BUILD)-v$(BLKI_VERSION)
